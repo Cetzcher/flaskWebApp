0de2c92b0c85a77575996dd43dd2240003e0d328
Pierre <pierre.rieger.rcc@gmali.com>
2018-01-08 13:39:57 +0100

    added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='webapp',
+    packages=['webapp'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        "flask-bootstrap",
+        "flask-pymongo"
+    ],
+)
